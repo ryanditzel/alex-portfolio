88dee88fbac4004bce7dc1b89d3e04218482695c
ryan.ditzel <ryand370@gmail.com>
2022-11-10 14:51:49 -0500

    built out Resume page

--- alex/src/App.js
+++ alex/src/App.js
@@ -1,21 +1,29 @@
-import "./App.css";
+import "./styles/App.css";
 import Header from "./components/Header";
 import Nav from "./components/Nav";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Photos from "./pages/Photos";
+import Resume from "./pages/Resume";
+import Videos from "./pages/Videos";
 
 const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <Nav />
-        <Header />
       </header>
       <main>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/photos" element={<Photos />} />
+          <Route path="/videos" element={<Videos />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
     </div>
---
+++ alex/src/pages/Resume.js
@@ -0,0 +1,247 @@
+import React from "react";
+
+const Resume = () => {
+  return (
+    <div className="resume-page">
+      <div className="resume-header">
+        <h2 className="resume-h2">Performance Experience</h2>
+        <button className="resume-button">Download Resume</button>
+      </div>
+      <div className="resume-h2">
+        <h2>Company Performance Experience</h2>
+      </div>
+      <div className="comp">
+        <h3 className="comp-title">Abarukas | Yoshito Sakuraba 2015-Present</h3>
+        <p className="comp-perf-name-left">
+          Anchorage - 2021
+          <br />
+          The Salty Land - 2018
+          <br />
+          Coming Home - 2018
+          <br />
+          Bernadac - 2016
+          <br />
+          The Master - 2016
+          <br />
+          No Man is an Island - 2016
+        </p>
+        <p className="comp-perf-loc-right">
+          Rabbit Island Foundations Presents at Rozsa Center for the Arts - MI
+          <br />
+          Festival Internacional de Danza Contemporánea de Ciudad de México -
+          Mexico City, Mexico
+          <br />
+          MASH International Contemporary Dance Festival - Jerusalem, Israel
+          <br />
+          MASDANZA Dance Festival - Canary Islands, Spain
+          <br />
+          Palm Desert Choreography Festival, McCallum Theater, CA
+          <br />
+          Nimbus Offline+ , BAM Fisher Theater - NYC
+          <br />
+          Jacob’s Pillow Dance Festival, Inside/ Out Theater - MA
+          <br />
+          IDACO Festival, Baruch College - NYC
+          <br />
+          Abarukas Presents: Bernadac, Gelsey Kirkland Theater, NYC
+          <br />
+          Fini Dance Festival, Ailey City Group Theater - NYC
+          <br />
+          APAP Showcases at Peridance Capezio Theater - NYC
+          <br />
+          West Fest Top Floor Program, Martha Graham Studio Theater - NYC
+          <br />
+        </p>
+      </div>
+      <div className="comp">
+        <h3 className="comp-title">
+          Eryc Taylor Dance | Eryc Taylor 2017-2022
+        </h3>
+        <p className="comp-perf-name-left">
+          Fracture/Refracture - 2022
+          <br />
+          Uncharted Territory - 2020
+          <br />
+          EARTH - 2019
+          <br />
+          Cycles - 2017
+          <br />
+          Chaise Lung - 2017
+          <br />
+          Song for a Cello and Piano - 2017
+          <br />
+          The Box - 2017
+          <br />
+        </p>
+        <p className="comp-perf-loc-right">
+          Avant Garde International Dance Festival - Mérida, Mexico
+          <br />
+          Bryant Park Contemporary Dance Festival - NYC
+          <br />
+          Chris Tanner Presents: Eryc Taylor Dance, HOWL Gallery, NYC
+          <br />
+          Dancers for Good, Guild Hall - East Hampton, NY
+          <br />
+          EARTH: NYC Premiere, Alchemical Studios - NYC
+          <br />
+          ETD NYC 2017 Season, Martha Graham Studio Theater - NYC
+        </p>
+      </div>
+      <div className="comp3">
+        <h3 className="comp-title">
+          Angie Moon Dance Theatre | Angie Moon 2016-2019
+        </h3>
+        <p className="comp-perf-name-left">
+          The Drowning Pine - 2019
+          <br />
+          Hunting Year - 2018
+          <br />
+          Vantablack - 2017
+          <br />
+          May Not/ Always - 2016
+          <br />
+          The Space is Seeing Us - 2016
+        </p>
+        <p className="comp-perf-loc-right">
+          Alumni Presents: The Drowning Pine, Jack Crystal Theater - NYC
+          <br />
+          inQUAD: Presented by Inclined Dance Project, Dixon Place - NYC
+          <br />
+          Triskelion Split Bill, Triskelion Arts Theater - NYC
+          <br />
+          Danspace Project, St. Marks Church in-the-Bowery - NYC
+        </p>
+      </div>
+      <div className="comp4">
+        <h3 className="comp-title">
+          Kizuna Dance Company | Cameron Mckinney 2014-2016
+        </h3>
+        <p className="comp-perf-name-left">
+          The Basic Bunch - 2016
+          <br />
+          Ukiyo - 2015
+          <br />
+          Monogatari - 2015
+          <br />
+          Other Lonely Seekers - 2014
+        </p>
+        <p className="comp-perf-loc-right">
+          Raja ‘Feather’ Kelly’s Lyrical Dances for a Lost Generation, Gelsey
+          Kirland Theater - NYC
+          <br />
+          The Moving Beauty Series, Secret Theater LIC - NYC
+          <br />
+          Never Before, Never Again Series, Triskelion Arts Theater - NYC
+          <br />
+          The Wasteland Showcase, 54 Theater at Shelter Studios - NYC
+        </p>
+      </div>
+      <div className="proj-perf-exp">
+        <h3>Project Performance Experience</h3>
+        <p className="proj-list">
+          Jonah Bokaer - 2022 - “Breaking the Rules” - Anniversary Gala for
+          Jonah Bokaer Foundation
+          <br />
+          Jonah Bokaer - 2019 - “Breaking the Rules” - Anniversary Gala for
+          Jonah Bokaer Foundation
+          <br />
+          Joseph Heitman - 2018 - “Bolt” - Bridge for Dance Next UP Series
+          <br />
+          Nick Rodrigues. - 2018 - “Igloo” - New Dance Alliance Presents:
+          Performance Mix Festival 32
+          <br />
+          Johanna LjungQvist-Brinson. - 2017 - “Lust & Suffering” - Fertile
+          Ground Festival at Greenspace
+          <br />
+          Joseph Victor Gonzales. - 2016 - “Other Echoes” - ADDC Presents: Topsy
+          <br />
+          Fairul Zahid & Yachao Zhu. - 2016 - “Axiomatic” - ADDC Presents: Topsy
+          <br />
+          Gregory Dolbashian - 2016 - “Avalanche 19” - Jack Crystal Theater
+          <br />
+          Netta Yerushalmy - 2016 - “PICTOGRAMS” - Second Avenue Dance Company
+          <br />
+          Rohan Bhargava - 2015 - “ALROCO” - WAXworks Series
+          <br />
+          Pamela Pietro - 2015 - “Shifting Rituals” - Building Bridges ASWARA:
+          NY-KUL
+          <br />
+          Kensaku Shinohara - 2015. - “Trio” - The Artists of the Nutcracker NYC
+          Gala
+          <br />
+          Giada Ferrone - 2014 - Nutcracker NYC{" "}
+        </p>
+      </div>
+      <div className="resume-h2">
+        <h2>CHOREOGRAPHIC EXPERIENCE</h2>
+        <p className="proj-list">
+          2019 - Choreographic Assistant to Eryc Taylor for “The Offering” -
+          Dancers for Good
+          <br />
+          2016 - “What S Mío” - WAXworks Series
+          <br />
+          2016 - Collaboration with Emma Pajewski “Sammie & ‘ise”* - SADC March
+          Concert
+          <br />
+          2015 - Choreographic Assistant to Pamela Pietro for “Shifting Rituals”
+          - Building Bridges ASWARA: NY-KUL
+          <br />
+          2015 - “Fight like a Girl” - SADC October Concert
+          <br />
+          2015 - Collaboration with Xenia Mansour & Lukas Dellios “Hiking
+          Through Mmzar’Lax Slar”** - 2015 - Scene & Unforscene Series
+          <br />
+          2015 - “Genus Differentia” - TSotA Showing 5
+        </p>
+      </div>
+      <div className="resume-h2">
+        <h2>Special Skills</h2>
+        <p>
+          Singing (1st Soprano), Acting, Painting, Photography, Digital Design,
+          Adobe Photoshoot & Premiere
+        </p>
+      </div>
+      <div className="resume-h2">
+        <h2>Education</h2>
+        <h3>Tisch School of the Arts - BFA Dance</h3>
+        <p>
+          2013 - 2016
+          <br />
+          Studied under Pamela Pietro, Rashaun Mitchell, Giada Ferrone, Cherylyn
+          Lavagnino, Jolinda Menendez, Joy Kellman, Nicole Wolcott, Jeremy
+          Nelson, Selina Chau, Kay Cummings, Lynn Martin, William Moulton, Allen
+          Fogelsanger, Susan Hamburger, Tara Marie Perri
+        </p>
+        <h3>Doug Varone Summer Intensive</h3>
+        <p>
+          Summer of 2015
+          <br />
+          Studied under Doug Varone, Alex Springer, Xan Burley, Hollis Bartlett,
+          Casey Loomis & Adriane Fang
+        </p>
+        <h3>Bates Dance Festival</h3>
+        <p>
+          Summer of 2013
+          <br />
+          Studied under Nancy Stark Smith, Autumn Eckman with Masterclasses by
+          Doug Varone & Dancers
+        </p>
+        <h3>University of Colorado at Boulder</h3>
+        <p>
+          2012 - 2013
+          <br />
+          Studied under Erika Randall, Donna Mejia, Mecca Madyun, Angie Hauser,
+          Chris Aiken
+        </p>
+        <h3>Turning Pointe Dance Academy</h3>
+        <p>
+          1999 - 2012
+          <br />
+          Studied under Michele Schulthies-Privratsky, Ashlee Schneider, Paul
+          Michael Gibbs, Kamy Henson, Kara Kunz, Jesse Kahat
+        </p>
+      </div>
+    </div>
+  );
+};
+export default Resume;
